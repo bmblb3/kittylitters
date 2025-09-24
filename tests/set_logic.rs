7f58c497159f20128e24f189fca9799d8b5fec10
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-24 11:31:27 +0000

    feat: convert to windows

--- tests/set_logic.rs
+++ tests/set_logic.rs
@@ -1,4 +1,7 @@
-use kittylitters::set_logic::{Operation, Solver};
+use kittylitters::{
+    set_logic::{Operation, Solver},
+    windows::Window,
+};
 
 #[rstest::rstest]
 #[case(
@@ -15,7 +18,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b"],
     vec!["X/b", "X/a"],
     vec![
-        Operation::GoTo(&"X/a"),
+        Operation::GoTo("X/a"),
         Operation::MoveWindowForward(1),
     ]
 )]
@@ -23,7 +26,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b"],
     vec!["X/a"],
     vec![
-        Operation::GoTo(&"X/b"),
+        Operation::GoTo("X/b"),
         Operation::CloseWindow
     ]
 )]
@@ -31,7 +34,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b"],
     vec!["X/b"],
     vec![
-        Operation::GoTo(&"X/a"),
+        Operation::GoTo("X/a"),
         Operation::CloseWindow
     ]
 )]
@@ -39,15 +42,15 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a"],
     vec!["X/a", "X/b"],
     vec![
-        Operation::GoTo(&"X/a"),
-        Operation::NewWindow(&"X/b"),
+        Operation::GoTo("X/a"),
+        Operation::NewWindow("X/b"),
     ]
 )]
 #[case(
     vec!["X/a", "X/b", "X/c"],
     vec!["X/a", "X/c"],
     vec![
-        Operation::GoTo(&"X/b"),
+        Operation::GoTo("X/b"),
         Operation::CloseWindow
     ]
 )]
@@ -55,9 +58,9 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a"],
     vec!["X/b"],
     vec![
-        Operation::GoTo(&"X/a"),
-        Operation::NewWindow(&"X/b"),
-        Operation::GoTo(&"X/a"),
+        Operation::GoTo("X/a"),
+        Operation::NewWindow("X/b"),
+        Operation::GoTo("X/a"),
         Operation::CloseWindow,
     ]
 )]
@@ -65,7 +68,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/a", "X/b"],
     vec![
-        Operation::GoTo(&"X/c"),
+        Operation::GoTo("X/c"),
         Operation::CloseWindow,
     ]
 )]
@@ -73,7 +76,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/a", "X/c", "X/b"],
     vec![
-        Operation::GoTo(&"X/b"),         // vec!["X/a", [X/b], "X/c"]
+        Operation::GoTo("X/b"),          // vec!["X/a", [X/b], "X/c"]
         Operation::MoveWindowForward(1), // vec!["X/a", "X/c", [X/b]]
     ]
 )]
@@ -81,7 +84,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/b", "X/a", "X/c"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b", "X/c"]
         Operation::MoveWindowForward(1), // vec!["X/b", [X/a], "X/c"]
     ]
 )]
@@ -89,7 +92,7 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/b", "X/c", "X/a"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b", "X/c"]
         Operation::MoveWindowForward(2), // vec!["X/b", [X/a], "X/c"]
                                          // vec!["X/b", "X/c", [X/a]]
     ]
@@ -98,10 +101,10 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/c", "X/b", "X/a"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b", "X/c"]
         Operation::MoveWindowForward(2), // vec!["X/b", [X/a], "X/c"]
                                          // vec!["X/b", "X/c", [X/a]]
-        Operation::GoTo(&"X/b"),         // vec![[X/b], "X/c", "X/a"]
+        Operation::GoTo("X/b"),          // vec![[X/b], "X/c", "X/a"]
         Operation::MoveWindowForward(1), // vec!["X/c", [X/b], "X/a"]
     ]
 )]
@@ -109,12 +112,12 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/c", "X/a", "X/b"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b", "X/c"]
         Operation::MoveWindowForward(1), // vec!["X/b", [X/a], "X/c"]
-        Operation::GoTo(&"X/b"),         // vec![[X/b], "X/a", "X/c"]
+        Operation::GoTo("X/b"),          // vec![[X/b], "X/a", "X/c"]
         Operation::MoveWindowForward(2), // vec!["X/a", [X/b], "X/c"]
                                          // vec!["X/a", "X/c", [X/b]]
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/c", "X/b"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/c", "X/b"]
         Operation::MoveWindowForward(1), // vec!["X/c", [X/a], "X/b"]
 
     ]
@@ -123,11 +126,11 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b"],
     vec!["X/c", "X/a"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b"]
-        Operation::NewWindow(&"X/c"),    // vec!["X/a", [X/c], "X/b"]
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/c", "X/b"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b"]
+        Operation::NewWindow("X/c"),     // vec!["X/a", [X/c], "X/b"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/c", "X/b"]
         Operation::MoveWindowForward(1), // vec!["X/c", [X/a], "X/b"]
-        Operation::GoTo(&"X/b"),         // vec!["X/c", "X/a", [X/b]]
+        Operation::GoTo("X/b"),          // vec!["X/c", "X/a", [X/b]]
         Operation::CloseWindow           // vec!["X/c", "X/a"]
     ]
 )]
@@ -135,19 +138,41 @@ use kittylitters::set_logic::{Operation, Solver};
     vec!["X/a", "X/b", "X/c"],
     vec!["X/d", "X/a", "X/b"],
     vec![
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/b", "X/c"]
-        Operation::NewWindow(&"X/d"),    // vec!["X/a", [X/d], "X/b", "X/c"]
-        Operation::GoTo(&"X/a"),         // vec![[X/a], "X/d", "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/b", "X/c"]
+        Operation::NewWindow("X/d"),     // vec!["X/a", [X/d], "X/b", "X/c"]
+        Operation::GoTo("X/a"),          // vec![[X/a], "X/d", "X/b", "X/c"]
         Operation::MoveWindowForward(1), // vec!["X/d", [X/a], "X/b", "X/c"]
-        Operation::GoTo(&"X/c"),         // vec!["X/d", "X/a", "X/b", [X/c]]
+        Operation::GoTo("X/c"),          // vec!["X/d", "X/a", "X/b", [X/c]]
         Operation::CloseWindow           // vec!["X/d", "X/a", "X/b"]
     ]
 )]
 fn test_cases(
     #[case] existing: Vec<&str>,
     #[case] desired: Vec<&str>,
-    #[case] expected: Vec<Operation<&&str>>,
+    #[case] expected: Vec<Operation<&str>>,
 ) {
-    let actual = Solver::derive_operations(&existing, &desired);
-    assert_eq!(expected, actual)
+    let existing: Vec<Window> = existing
+        .iter()
+        .map(|title| Window::new(title.to_string()))
+        .collect();
+    let desired: Vec<Window> = desired
+        .iter()
+        .map(|title| Window::new(title.to_string()))
+        .collect();
+
+    let operations_on_windows = Solver::derive_operations(&existing, &desired);
+
+    let mut operations: Vec<Operation<&str>> = Vec::new();
+    for op in operations_on_windows {
+        match op {
+            Operation::GoTo(win) => operations.push(Operation::GoTo(&win.title)),
+            Operation::MoveWindowForward(usize) => {
+                operations.push(Operation::MoveWindowForward(usize))
+            }
+            Operation::CloseWindow => operations.push(Operation::CloseWindow),
+            Operation::NewWindow(win) => operations.push(Operation::NewWindow(&win.title)),
+        }
+    }
+
+    assert_eq!(expected, operations)
 }
