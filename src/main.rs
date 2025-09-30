a0fe415dc96a6567488de5348a102b085c49a5b5
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-30 11:57:24 +0000

    feat: add editor step

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ fn main() -> color_eyre::Result<()> {
 
     let home_dir = std::env::var("HOME").expect("HOME environment variable should be set");
     let config_path = format!("{}/.config/kitty/session.yml", home_dir);
+
+    let editor = if let Ok(e) = std::env::var("EDITOR") {
+        e
+    } else {
+        "vi".to_string()
+    };
+    Command::new(editor).arg(&config_path).status()?;
+
     let desired_windows = read_session_yml(&config_path)
         .expect("Expect a valid yml file at ~/.config/kitty/session.yml");
     let desired_windows: Vec<Window> = Window::from_instructions(&desired_windows);
